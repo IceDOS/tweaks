96a1f354a9716901daa81cd7bf65d24985d4153d
IceDBorn <git.outsider841@simplelogin.fr>
2026-01-07 17:35:27 +0200

    feat(gaming): add udev rules for various controllers

--- modules/gaming/icedos.nix
+++ modules/gaming/icedos.nix
@@ -4,36 +4,42 @@
   outputs.nixosModules =
     { ... }:
     [
-      {
-        boot = {
-          kernelModules = [ "ntsync" ];
+      (
+        { pkgs, ... }:
+        {
+          boot = {
+            kernelModules = [ "ntsync" ];
 
-          kernelParams = [
-            "clearcpuid=514" # Disables UMIP which fixes certain games from crashing on launch
+            kernelParams = [
+              "clearcpuid=514" # Disables UMIP which fixes certain games from crashing on launch
+            ];
+
+            kernel.sysctl = {
+              "kernel.split_lock_mitigate" = 0; # Fixes some games from stuttering
+              "vm.max_map_count" = 1048576; # Fixes crashes or start-up issues for games
+            };
+          };
+
+          security.pam.loginLimits = [
+            {
+              domain = "*";
+              type = "hard";
+              item = "memlock";
+              value = "2147483648";
+            }
+
+            {
+              domain = "*";
+              type = "soft";
+              item = "memlock";
+              value = "2147483648";
+            }
           ];
 
-          kernel.sysctl = {
-            "kernel.split_lock_mitigate" = 0; # Fixes some games from stuttering
-            "vm.max_map_count" = 1048576; # Fixes crashes or start-up issues for games
-          };
-        };
-
-        security.pam.loginLimits = [
-          {
-            domain = "*";
-            type = "hard";
-            item = "memlock";
-            value = "2147483648";
-          }
-
-          {
-            domain = "*";
-            type = "soft";
-            item = "memlock";
-            value = "2147483648";
-          }
-        ];
-      }
+          # Udev rules to make supported controllers available with user-grade permissions
+          services.udev.packages = with pkgs; [ game-devices-udev-rules ];
+        }
+      )
     ];
 
   meta.name = "gaming";
